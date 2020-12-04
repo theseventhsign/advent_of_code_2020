66e98bebc2ba5fb88e9c8861a31a31264a2eb723
Isabella Velásquez <ivelasq@gmail.com>
2020-12-03 22:09:44 -0600

    update code

--- day_03/ivelasq/day03.R
+++ day_03/ivelasq/day03.R
@@ -1,48 +1,46 @@
 # Advent of Code - Day 03
 
-# Libraries ---------------------------------------------------------------
-
-library(tidyverse)
-
 # Data --------------------------------------------------------------------
 
 dat <-
-  as.matrix(read.fwf(here::here("day_03", "trees.txt"),
-                     widths = rep(1, 31),
-                     comment.char = ""))
+  as.matrix(read.fwf(
+    here::here("day_03", "trees.txt"),
+    widths = rep(1, 31),
+    comment.char = ""
+  ))
 
 # Functions ---------------------------------------------------------------
 
-# this is to create a matrix that accommodates all the movement that
+# this is to create a mat that accommodates all the movement that
 # you have to go through to get to the bottom right
 
-get_encounters <- function(matrix, num_right, num_down){
+trees_get <- function(mat, n_right, n_down){
   
-  repeats <- ceiling(num_right * nrow(matrix) / ncol(matrix))
+  repeats <- ceiling(n_right * nrow(mat) / ncol(mat))
   
-  new_mat <- do.call(cbind, replicate(repeats, matrix, simplify = F))
+  new_mat <- do.call(cbind, replicate(repeats, mat, simplify = F))
 
   encounters <- c()
   
-  n <- ceiling(nrow(matrix) / num_down) - 1
+  n <- ceiling(nrow(mat) / n_down) - 1
   
   for(i in 1:n){
     
-    encounters[i] <- new_mat[num_down * i + 1, num_right * i + 1]
+    encounters[i] <- new_mat[n_down * i + 1, n_right * i + 1]
       
   }
   
-  print(sum(as.numeric(str_count(encounters, "#")))) # avoid integer overflow later
+  print(sum(as.numeric(stringr::str_count(encounters, "#")))) # avoid integer overflow later
   
 }
 
-get_encounters_again <- function(matrix, slopes){
+prod_trees <- function(mat, slopes){
   
   n_trees <- c()
   
   for(i in 1:nrow(slopes)){
     
-    n_trees[i] <- get_encounters(matrix, slopes[i, 1], slopes[i, 2])
+    n_trees[i] <- trees_get(mat, slopes[i, 1], slopes[i, 2])
   }
   
   print(prod(n_trees))
@@ -53,7 +51,7 @@ get_encounters_again <- function(matrix, slopes){
 
 # Part 01
 
-get_encounters(dat, 3, 1)
+trees_get(dat, 3, 1)
 
 # Part 02
 
@@ -65,3 +63,5 @@ entries <-
            1, 2),
          nrow = 5,
          byrow = T)
+
+prod_trees(dat, entries)
